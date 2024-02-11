e16e13f86461f667d92dd086564d0fc2a38bff32
vancy12 <saanvishet12@gmail.com>
2024-02-11 23:53:21 +0530

    all done

---
+++ main.c
@@ -0,0 +1,75 @@
+#include "tree.c"
+
+int main() {
+
+    tree T1;
+    T1 = initBST(T1, 7);
+    printf("size of tree: %d\n", T1.size);
+
+    // T1 = insertBST(T1, 20);
+    // T1 = insertBST(T1, 30);
+    // T1 = insertBST(T1, 40);
+    // T1 = insertBST(T1, 25);
+    // T1 = insertBST(T1, 10);
+    // T1 = insertBST(T1, 15);
+    // T1 = insertBST(T1, 5);
+    
+    int choice, data;
+    do {
+        printf("\nMenu:\n");
+        printf("1. Insert into BST\n");
+        printf("2. Recursive Inorder Traversal\n");
+        printf("3. Recursive Preorder Traversal\n");
+        printf("4. Recursive Postorder Traversal\n");
+        printf("5. Level-wise Traversal\n");
+        printf("6. Check if BST is Complete\n");
+        printf("7. Remove Node\n");
+        printf("8. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                T1 = insertBST(T1, data);
+                break;
+            case 2:
+                printf("Inorder Traversal: ");
+                inorder(T1);
+                printf("\n");
+                break;
+            case 3:
+                printf("Preorder Traversal: ");
+                preorder(T1);
+                printf("\n");
+                break;
+            case 4:
+                printf("Postorder Traversal: ");
+                postorder(T1);
+                printf("\n");
+                break;
+            case 5:
+                printf("Level-wise Traversal: ");
+                levelwise(T1);
+                printf("\n");
+                break;
+            case 6:
+                isComplete(T1);
+                break;
+            case 7:
+                printf("Enter node to be removed: ");
+                scanf("%d", &data);
+                removeNode(T1, data);
+                break;
+            case 8:
+                free(T1.root);
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice! Please enter a number between 1 and 7.\n");
+        }
+    } while(choice != 8);
+ 
+    return 0;
+}
---
+++ tree.c
@@ -0,0 +1,188 @@
+// Online C compiler to run C program online
+#include "header.h"
+
+tree initBST(tree R, int length){
+    R.root = (int*)malloc(sizeof(int)*length);
+    R.size = length;
+    return R;
+}
+
+void initStack(stack* S, int length){
+    S->top = -1;
+    S->size = length;
+    S->array = (int*)malloc(sizeof(int)*length);
+}
+
+
+tree insertBST(tree R, int data){
+    int idx = 0;
+    
+    // for(int i =0; i < R.size; i++){
+    //  R.root[i] = 0;
+    // }
+    
+    while(idx < R.size){
+        if(R.root[idx] == 0){
+            R.root[idx] = data;
+            break;
+        }
+        else if(data < R.root[idx]){
+            idx = 2*idx+1;
+        }
+        else if(data > R.root[idx]){
+            idx = 2*idx+2;
+        }
+    }
+    return R;
+}
+
+void levelwise(tree R){
+    for(int i = 0; i < R.size; i++){
+        if(R.root[i] != 0)
+            printf("%d ", R.root[i]);
+    }
+    return;
+}
+
+void isComplete(tree R){
+    int count = 0, level = 1;
+    for(int i = 0; i < R.size; i++){
+        if(R.root[i] != 0)
+            count++;
+    }
+
+    int calculate = pow(2, level)-1;
+    while(calculate < R.size){
+        calculate = pow(2, level)-1;
+        if (count == calculate){
+            printf("\nComplete BST\n");
+            return;
+        }
+        if(count > calculate){
+            level++;
+            continue;
+        }
+        if(count < calculate){
+            printf("\nIncomplete BST\n");
+            return;
+        }
+    }
+}
+
+int isFull(stack S){
+    return(S.top == S.size);
+}
+int isEmpty(stack S){
+    return(S.top == -1);
+}
+
+void push(stack* S, int index){
+    if(isFull(*S))
+        return;
+    S->top += 1;
+    S->array[S->top] = index;
+    return;
+}
+
+int pop(stack* S){
+    if(isEmpty(*S))
+        return 0;
+    int p = S->array[S->top];
+    S->top -= 1;
+    // printf("Value of top is:%d", S->top);
+    return p;
+}
+int peek(stack S){
+    if(isEmpty(S))
+        return 0;
+    return(S.array[S.top]);
+}
+
+void inorder(tree R){
+    stack store;
+    initStack(&store, R.size);
+    int flag = 1;
+    int p = 0;
+    while(flag){
+        if(p < R.size){
+            // printf("value being pushed:%d \n", p);
+            push(&store, p);
+            p = 2*p + 1;
+            // printf("new index: %d\n", p);
+        }
+        
+        else{
+           if(!isEmpty(store)){
+            p = pop(&store);
+            // printf("value being popped: %d\n", p);
+            if(R.root[p] != 0){
+                printf("%d ", R.root[p]);
+            }
+            p = 2*p + 2;
+           }
+           else{
+               flag = 0;
+           }
+           
+        }
+    }
+}
+
+void postorder(tree R){
+    stack a, b;
+    int p = 0;
+    initStack(&a, R.size);
+    initStack(&b, R.size);
+    push(&a, p);
+    // printf("outside while loop\n");
+    while(!isEmpty(a)){
+        // printf("inside while loop\n");
+        // printf("value of p:%d", p);
+        p = pop(&a);
+        // a.top --;
+        push(&b, p);
+        if(R.root[2*p+1] != 0 && 2*p+1 <= R.size){
+            push(&a, 2*p+1);
+        }
+        if(R.root[2*p+2] != 0 && 2*p+2 <= R.size){
+            push(&a, 2*p+2);
+        }
+    }
+    while(!isEmpty(b)){
+        if(R.root[peek(b)] != 0)
+            printf("%d ",R.root[pop(&b)]);
+    }
+}
+
+void preorder(tree R) {
+    stack a;
+    // root node --> index is 0
+    int p = 0;
+    initStack(&a, R.size);
+    push(&a, p);
+    
+    while(!isEmpty(a)){
+        p = pop(&a);
+        if(R.root[p] != 0) {
+            if(R.root[p] != 0)
+                printf("%d ", R.root[p]); // Visit current node
+            // Push right child first
+            if(R.root[2*p+2] != 0 && 2*p+2 < R.size)
+                push(&a, 2*p+2);
+            // Push left child
+            if(R.root[2*p+1] != 0 && 2*p+1 < R.size)
+                push(&a, 2*p+1);
+        }
+    }
+}
+
+void removeNode(tree R, int rem){
+    int i;
+    for(i = 0; i <= R.size; i++){
+        if (R.root[i] == rem){
+            R.root[i] = 0;
+            break;
+        }
+    }  
+}
+
